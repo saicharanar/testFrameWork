2316053349af932e2ece40fccf425dea1396cb18
SaiCharan A <99522580+saicharanar@users.noreply.github.com>
2022-05-04 18:29:15 +0530

    My testFramework

---
+++ testFlatten.js
@@ -0,0 +1,43 @@
+const Lib = require('./flatten.js');
+const flatten = Lib.flatten;
+const { deepEqual } = require('assert').strict;
+
+const makeTest = (desc, test) => {
+  return { desc, test }
+}
+
+const runTest = ({ desc, test }) => {
+  let status = '✅';
+  let errorMessage;
+  try {
+    test();
+  } catch (error) {
+    status = '❌';
+    errorMessage = error.message;
+  } finally {
+    console.log(status, '-', desc);
+    if (errorMessage) {
+      console.log(errorMessage);
+    }
+  }
+}
+
+const runTests = (tests) => {
+  tests.forEach(runTest);
+}
+
+const tests = [
+  makeTest('Empty array', (desc) => {
+    const actual = flatten([]);
+    const expected = [];
+    return deepEqual(actual, expected, desc);
+  }),
+  makeTest('Single index nested array', (desc) => {
+    return deepEqual(flatten([[1]]), [1, 2], desc);
+  }),
+  makeTest("multi index nested array", (desc) => {
+    return deepEqual(flatten([[[1], 2]]), [1, 2], desc);
+  }),
+];
+
+runTests(tests);
