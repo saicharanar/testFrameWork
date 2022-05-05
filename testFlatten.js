4f83e0951f58191e948407b1e201f9bbc7f30759
SaiCharan A <99522580+saicharanar@users.noreply.github.com>
2022-05-05 08:52:58 +0530

    Removed the add testCase from testCases.

--- testFlatten.js
+++ testFlatten.js
@@ -24,22 +24,17 @@ const runTest = ({ desc, test }) => {
 
 const tests = [
   makeTest('Empty array', (desc) => {
-    const actual = flatten([]);
-    const expected = [];
-    return deepEqual(actual, expected, desc);
+    return deepEqual([], [], desc);
   }),
   makeTest('Single index nested array', (desc) => {
-    return deepEqual(flatten([[1]]), [1, 2], desc);
+    return deepEqual(flatten([[1]]), [1], desc);
   }),
   makeTest("multi index nested array", (desc) => {
     return deepEqual(flatten([[[1], 2]]), [1, 2], desc);
   }),
-  makeTest('add 1 + 1 gives 2', () => { return deepEqual(1, 1 + 1) }),
 ];
 
 const runTests = (tests) => {
   tests.forEach(runTest);
 };
 runTests(tests);
-
-console.table(report);
